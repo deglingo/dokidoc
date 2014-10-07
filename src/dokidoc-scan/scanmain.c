841cf4d0bec3022803e5055b6a22aad664838bd1
deglingo <ahmidmefes@gmail.com>
2014-10-07 21:33:38 +0200

    added the C scanner

--- src/dokidoc-scan/scanmain.c
+++ src/dokidoc-scan/scanmain.c
@@ -1,11 +1,40 @@
 /* scanmain.c -
  */
 
+#include "dokidoc-base/dokidoc-sysconf.h"
+
 #include <clog.h>
 #include <glib.h>
+#include <gmodule.h>
 #include <glob.h>
 #include <string.h>
 #include <stdlib.h>
+
+
+
+typedef struct _ScannerClass ScannerClass;
+typedef struct _Scanner Scanner;
+
+typedef void (* ProcessFunc) ( Scanner *scanner,
+                               GError **error );
+
+
+
+struct _ScannerClass
+{
+  gchar *name;
+  gchar *module_path;
+  GModule *module;
+  ProcessFunc process;
+};
+
+
+
+struct _Scanner
+{
+  ScannerClass *cls;
+  gchar *filename;
+};
 
 
 
@@ -44,6 +73,56 @@ static GOptionEntry OPTION_ENTRIES[] =
 
 
 
+/* scanner_class_new:
+ */
+static ScannerClass *scanner_class_new ( const gchar *name,
+                                         const gchar *module_path )
+{
+  ScannerClass *cls;
+  cls = g_new0(ScannerClass, 1);
+  cls->name = g_strdup(name);
+  cls->module_path = g_strdup(module_path);
+  return cls;
+}
+
+
+
+/* scanner_new:
+ */
+static Scanner *scanner_new ( ScannerClass *cls,
+                              gchar *filename )
+{
+  Scanner *scanner;
+  if (!cls->module)
+    {
+      gpointer process;
+      CL_DEBUG("opening module: '%s'", cls->module_path);
+      if (!(cls->module = g_module_open(cls->module_path, G_MODULE_BIND_LOCAL)))
+        CL_ERROR("could not load module!");
+      if (!(g_module_symbol(cls->module, "dokidoc_scanner_process", &process)))
+        CL_ERROR("could not find 'dokidoc_scanner_process' symbol in module");
+      cls->process = process;
+    }
+  scanner = g_new0(Scanner, 1);
+  scanner->cls = cls;
+  scanner->filename = g_strdup(filename);
+  return scanner;
+}
+
+
+
+/* scanner_process:
+ */
+static void scanner_process ( Scanner *scanner )
+{
+  GError *error = NULL;
+  scanner->cls->process(scanner, &error);
+  if (error)
+    CL_ERROR("process error: %s", error->message);
+}
+
+
+
 /* get_option_context:
  */
 static GOptionContext *get_option_context ( void )
@@ -66,13 +145,13 @@ static void _get_source_bases ( Config *config,
   GError *error = NULL;
   if (!(bases = g_key_file_get_string_list(kfile, "DEFAULT", "source_bases", &n_bases, &error)))
     CL_ERROR("source_bases required");
-  CL_DEBUG("%d source bases:", n_bases);
+  /* CL_DEBUG("%d source bases:", n_bases); */
   for (n = 0; n < n_bases; n++)
     {
       gchar *path = realpath(g_strstrip(bases[n]), NULL);
       SourceBase *source_base = g_new0(SourceBase, 1);
       source_base->path = g_strdup(path);
-      CL_DEBUG(" - '%s'", source_base->path);
+      /* CL_DEBUG(" - '%s'", source_base->path); */
       free(path);
       config->source_bases = g_list_append(config->source_bases, source_base);
     }
@@ -139,13 +218,13 @@ static void _get_sources ( Config *config,
   GError *error = NULL;
   if (!(sources = g_key_file_get_string_list(kfile, "DEFAULT", "sources", &n_sources, &error)))
     CL_ERROR("sources required");
-  CL_DEBUG("found %d sources:", n_sources);
+  /* CL_DEBUG("found %d sources:", n_sources); */
   /* glob patterns */
   for (n = 0; n < n_sources; n++)
     {
       gint r;
       gchar *pattern = g_strstrip(sources[n]);
-      CL_DEBUG(" - '%s'", pattern);
+      /* CL_DEBUG(" - '%s'", pattern); */
       if ((r = glob(pattern, GLOB_APPEND | GLOB_NOSORT, NULL, &vector)) != 0)
         CL_ERROR("glob failed: %d", r);
     }
@@ -164,12 +243,12 @@ static void _get_sources ( Config *config,
       free(full_path);
     }
   config->source_files = g_list_sort(config->source_files, _sort_source_files);
-  {
-    GList *l;
-    CL_DEBUG("found %d files:", g_list_length(config->source_files));
-    for (l = config->source_files; l; l = l->next)
-      CL_DEBUG(" - '%s'", ((SourceFile *) (l->data))->path);
-  }
+  /* { */
+  /*   GList *l; */
+  /*   CL_DEBUG("found %d files:", g_list_length(config->source_files)); */
+  /*   for (l = config->source_files; l; l = l->next) */
+  /*     CL_DEBUG(" - '%s'", ((SourceFile *) (l->data))->path); */
+  /* } */
 }
 
 
@@ -193,6 +272,20 @@ static void parse_config ( Config *config,
 
 
 
+/* _process_file:
+ */
+static void _process_file ( Config *config,
+                            ScannerClass *cls,
+                            SourceFile *file )
+{
+  Scanner *scanner;
+  CL_DEBUG("pocessing file: '%s'", file->path);
+  scanner = scanner_new(cls, file->path);
+  scanner_process(scanner);
+}
+
+
+
 /* main:
  */
 int main ( gint argc,
@@ -201,6 +294,7 @@ int main ( gint argc,
   GOptionContext *option_context;
   GError *error;
   Config *config;
+  GList *l;
   CL_DEBUG("hello!");
   /* parse the command line */
   option_context = get_option_context();
@@ -212,5 +306,11 @@ int main ( gint argc,
     CL_ERROR("too many args");
   config = g_new0(Config, 1);
   parse_config(config, argv[1]);
+  /* process */
+  {
+    ScannerClass *cls = scanner_class_new("C", g_build_filename(DOKIDOC_SCANNERSDIR, "dokidoc-scanner-c.so", NULL));
+    for (l = config->source_files; l; l = l->next)
+      _process_file(config, cls, l->data);
+  }
   return 0;
 }
---
+++ src/dokidoc-scan/scanner-c/scannercmain.c
@@ -0,0 +1,15 @@
+/* scannercmain.c -
+ */
+
+#include <glib.h>
+#include <clog.h>
+
+
+
+/* dokidoc_scanner_process:
+ */
+void dokidoc_scanner_process ( gpointer scanner /* [fixme] */,
+                               GError **error )
+{
+  CL_DEBUG("SCAN PROCESS...");
+}
